cef3d9ec9adb08b14a2eeba7dc8b8838ad08caf1
Shubham Kumar <kumaruikeshubham@gmail.com>
2024-12-08 18:52:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Add to Cart functionality
+document.querySelectorAll('.product-card button').forEach((button) => {
+    button.addEventListener('click', () => {
+        alert('Product added to cart!');
+    });
+});
